6058e1d3be92c5dcbd7f12f8c2f3e27f0607009b
Yocheved <cheved.stern@gmail.com>
2018-12-17 15:30:57 +0200

    refactor onDragEnd

--- client/src/components/TableManager.js
+++ client/src/components/TableManager.js
@@ -44,7 +44,7 @@ const styles = theme => ({
   checkbox: {
     width: 30,
     marginLeft: 10
-  }, 
+  },
   divider: {
     marginBottom: 20
   }
@@ -76,118 +76,92 @@ export class TableManager extends Component {
     };
   }
 
-  onDragEnd = result => {
-    let currentEvent = this.props.store.user.events[this.props.store.eventIndex];
-
-    if (result.destination === null)
-      return;
-
-    if ((result.destination.droppableId === result.source.droppableId &&
-      result.destination.index === result.source.index) ||
-      (result.source.droppableId === "-1" && result.destination.droppableId === "-1")) {
-      return;
-    }
-
-
-
-    //from table0 to table
-    if (result.source.droppableId === "-1") {
-      const destinationIndex = currentEvent.tables.findIndex(table => table._id === result.destination.droppableId);
-
-      let finish = currentEvent.tables[destinationIndex];
-      /*  let unseatedGuests = currentEvent.guests.filter(guest => guest.seated === false);
-       let myGuest = unseatedGuests[result.source.index]; */
-
-      /* let guestSourceIndex = currentEvent.guests.findIndex(guest => guest._id === myGuest._id); */
-      let newGuests = Array.from(currentEvent.guests);
-      let myGuest = newGuests.find(guest => guest._id === result.draggableId);
-      myGuest.seated = true;
-
-
-      const finishGuests = Array.from(finish.guests);
-      finishGuests.splice(result.destination.index, 0, myGuest._id);
-      const newDestinationTable = {
-        ...finish,
-        guests: finishGuests
-      }
-
-      this.props.store.updateGuests(newGuests);
-      this.props.store.updateTable(newDestinationTable, destinationIndex);
-      axios.post('/beOurGuest/updateGuestsInTable/', newDestinationTable)
-        .then(response => {
-          // console.log(response);
-        }).then(res => {
-          axios.post('/beOurGuest/updateEventGuest/', myGuest)
-            .then(res1 => {
-              // console.log(res1);
-            });
-        })
-        .catch(err => console.log('Error: ', err));
-      return;
-    }
-
-    //from table to table0
-    if (result.destination.droppableId === "-1") {
-      const sourceIndex = currentEvent.tables.findIndex(table => table._id === result.source.droppableId);
-      let start = Object.assign(currentEvent.tables[sourceIndex]);
-      const newGuests = Array.from(start.guests);
-      let myGuest = newGuests.splice(result.source.index, 1);
-      start.guests = newGuests;
-      let index = currentEvent.guests.findIndex(guest => myGuest[0] === guest._id);
-      let eventGuests = Array.from(currentEvent.guests);
-      eventGuests[index].seated = false;
-
-
-
-      this.props.store.updateTable(start, sourceIndex);
-      this.props.store.updateGuests(eventGuests);
-
-      axios.post('/beOurGuest/updateGuestsInTable/', start)
-        .then(response => {
-          // console.log(response);
-        }).then(res => {
-          axios.post('/beOurGuest/updateEventGuest/', eventGuests[index])
-            .then(res1 => {
-              console.log(res1);
-            });
-        })
-        .catch(err => console.log('Error: ', err));
-
-      return;
-    }
-
-    //MOVING BETWEEN TABLES
-    const sourceIndex = currentEvent.tables.findIndex(table => table._id === result.source.droppableId);
+  unseatedToSeated = (result, currentEvent) => {
     const destinationIndex = currentEvent.tables.findIndex(table => table._id === result.destination.droppableId);
 
-    const start = currentEvent.tables[sourceIndex];
-    const finish = currentEvent.tables[destinationIndex];
+    let finish = currentEvent.tables[destinationIndex];
+    /*  let unseatedGuests = currentEvent.guests.filter(guest => guest.seated === false);
+     let myGuest = unseatedGuests[result.source.index]; */
 
-    //Moving within a tables
-    if (start === finish) {
-      const newGuests = Array.from(start.guests);
-      let myGuest = newGuests.splice(result.source.index, 1);
-      newGuests.splice(result.destination.index, 0, myGuest[0]);
+    /* let guestSourceIndex = currentEvent.guests.findIndex(guest => guest._id === myGuest._id); */
+    let newGuests = Array.from(currentEvent.guests);
+    let myGuest = newGuests.find(guest => guest._id === result.draggableId);
+    myGuest.seated = true;
 
 
-      const newTable = {
-        ...start,
-        guests: newGuests
-      };
-
-      let newTables = Array.from(currentEvent.tables);
-      newTables[sourceIndex] = newTable;
-
-      this.props.store.updateTable(newTable, sourceIndex);
-      axios.post('/beOurGuest/updateGuestsInTable/', newTable)
-        .then(response => {
-          // console.log(response);
-        })
-        .catch(err => console.log('Error: ', err));
-      return;
+    const finishGuests = Array.from(finish.guests);
+    finishGuests.splice(result.destination.index, 0, myGuest._id);
+    const newDestinationTable = {
+      ...finish,
+      guests: finishGuests
     }
 
-    //moving from one table to another
+    this.props.store.updateGuests(newGuests);
+    this.props.store.updateTable(newDestinationTable, destinationIndex);
+    axios.post('/beOurGuest/updateGuestsInTable/', newDestinationTable)
+      .then(response => {
+        // console.log(response);
+      }).then(res => {
+        axios.post('/beOurGuest/updateEventGuest/', myGuest)
+          .then(res1 => {
+            // console.log(res1);
+          });
+      })
+      .catch(err => console.log('Error: ', err));
+  }
+
+
+  seatedToUnseated = (result, currentEvent) => {
+    const sourceIndex = currentEvent.tables.findIndex(table => table._id === result.source.droppableId);
+    let start = Object.assign(currentEvent.tables[sourceIndex]);
+    const newGuests = Array.from(start.guests);
+    let myGuest = newGuests.splice(result.source.index, 1);
+    start.guests = newGuests;
+    let index = currentEvent.guests.findIndex(guest => myGuest[0] === guest._id);
+    let eventGuests = Array.from(currentEvent.guests);
+    eventGuests[index].seated = false;
+
+
+
+    this.props.store.updateTable(start, sourceIndex);
+    this.props.store.updateGuests(eventGuests);
+
+    axios.post('/beOurGuest/updateGuestsInTable/', start)
+      .then(response => {
+        // console.log(response);
+      }).then(res => {
+        axios.post('/beOurGuest/updateEventGuest/', eventGuests[index])
+          .then(res1 => {
+            console.log(res1);
+          });
+      })
+      .catch(err => console.log('Error: ', err));
+
+  }
+
+  stayInTable = (result, currentEvent, sourceIndex, start, finish) => {
+    const newGuests = Array.from(start.guests);
+    let myGuest = newGuests.splice(result.source.index, 1);
+    newGuests.splice(result.destination.index, 0, myGuest[0]);
+
+
+    const newTable = {
+      ...start,
+      guests: newGuests
+    };
+
+    let newTables = Array.from(currentEvent.tables);
+    newTables[sourceIndex] = newTable;
+
+    this.props.store.updateTable(newTable, sourceIndex);
+    axios.post('/beOurGuest/updateGuestsInTable/', newTable)
+      .then(response => {
+        // console.log(response);
+      })
+      .catch(err => console.log('Error: ', err));
+  }
+
+  tableToTable = (result, currentEvent, sourceIndex, destinationIndex, start, finish) => {
     const startGuests = Array.from(start.guests);
     let myGuest = startGuests.splice(result.source.index, 1);
     const newStart = {
@@ -217,6 +191,50 @@ export class TableManager extends Component {
       })
       .catch(err => console.log('Error: ', err));
     this.props.store.updateTables(newTables);
+  }
+
+  onDragEnd = result => {
+    let currentEvent = this.props.store.user.events[this.props.store.eventIndex];
+
+    if (result.destination === null)
+      return;
+
+    if ((result.destination.droppableId === result.source.droppableId &&
+      result.destination.index === result.source.index) ||
+      (result.source.droppableId === "-1" && result.destination.droppableId === "-1")) {
+      return;
+    }
+
+
+
+    //from table0 to table
+    if (result.source.droppableId === "-1") {
+      this.unseatedToSeated(result, currentEvent);
+      return;
+    }
+
+    //from table to table0
+    if (result.destination.droppableId === "-1") {
+      this.seatedToUnseated(result, currentEvent);
+      return;
+    }
+
+    //MOVING BETWEEN TABLES
+    const sourceIndex = currentEvent.tables.findIndex(table => table._id === result.source.droppableId);
+    const destinationIndex = currentEvent.tables.findIndex(table => table._id === result.destination.droppableId);
+
+    const start = currentEvent.tables[sourceIndex];
+    const finish = currentEvent.tables[destinationIndex];
+
+    //Moving within a table
+    if (start === finish) {
+      this.stayInTable(result, currentEvent, sourceIndex, start, finish);
+      return;
+    }
+
+    //moving from one table to another
+    this.tableToTable(result, currentEvent, sourceIndex, destinationIndex, start, finish);
+
   }
 
   handleChange = event => {
@@ -285,14 +303,14 @@ export class TableManager extends Component {
                 }
                 label="Only filter tables"
               />
-              
+
 
             </div>
 
           </div>
 
         </div>
-        <Divider  className={classes.divider}/>
+        <Divider className={classes.divider} />
 
         <TableList filteredCategories={this.state.name} onlyTables={this.state.checked} />
       </DragDropContext>
